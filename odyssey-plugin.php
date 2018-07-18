2b8e97eb303a688d8a5fa63e0f0fd702903b07a6
Xavier Roy <xavierroy@users.noreply.github.com>
2018-07-18 14:13:54 +0530

    Create odyssey-plugin.php

---
+++ odyssey-plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * @link              https://xavierroy.com
+ * @since             1.0.0
+ * @package           Odyssey_Plugin.php
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Odyssey - Site Enhancements
+ * Plugin URI:        https://github.com/xavierroy/odyssey-plugin/
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Xavier Roy
+ * Author URI:        https://xavierroy.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       odyssey-plugin.php
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+?>
