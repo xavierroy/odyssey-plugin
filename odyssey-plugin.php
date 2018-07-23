cdc4c3079a54306d06826a67977b46aa9151499a
Xavier Roy <xavierroy@users.noreply.github.com>
2018-07-23 12:35:26 +0530

    Fixed double declaration

--- odyssey-plugin.php
+++ odyssey-plugin.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Odyssey - Site Enhancements
  * Plugin URI:        https://github.com/xavierroy/odyssey-plugin/
  * Description:       Tweaks and hacks for this site...
- * Version:           1.0.7
+ * Version:           1.0.7-fix
  * Author:            Xavier Roy
  * Author URI:        https://xavierroy.com
  * License:           GPL-2.0+
@@ -80,14 +80,7 @@ if ( !function_exists('indieweb_check_webmention') ) {
 	}
 
 	add_filter('pre_comment_approved', 'indieweb_check_webmention', '99', 2);
-	function disable_self_trackback( &$links ) {
-  foreach ( $links as $l => $link )
-        if ( 0 === strpos( $link, get_option( 'home' ) ) )
-            unset($links[$l]);
-}
 
-add_action( 'pre_ping', 'disable_self_trackback' );
-}
 /* --2-- */
 
 
@@ -146,8 +139,6 @@ function filter_title_save_pre( $title ) {
 5. Disable Self Pingbacks
 Source: https://www.wpstuffs.com/disable-self-pingbacks/
 */
-add_filter( 'title_save_pre', 'filter_title_save_pre', 10, 1 );
-
 
 function disable_self_trackback( &$links ) {
   foreach ( $links as $l => $link )
